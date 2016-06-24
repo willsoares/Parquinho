11ca2c6084926ce43c103ea0b8b9682f6c05f7c2
huguera10 <a14014@bcc.unifal-mg.edu.br>
2016-06-24 12:39:03 -0300

    correção de bugs

--- ParqueDiversoes/src/gui/functions/AtualizaEstoque.java
+++ ParqueDiversoes/src/gui/functions/AtualizaEstoque.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gui.funcions;
+package gui.functions;
 
 import banco.BDController;
 import java.sql.SQLException;
